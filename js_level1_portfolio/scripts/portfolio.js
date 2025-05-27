f4197f3cbb75e478dbc97bea45b31b3333dd8737
jiyun <jyiun27@gmail.com>
2025-05-27 16:08:48 +0900

    프로젝트 view 완성

--- js_level1_portfolio/scripts/portfolio.js
+++ js_level1_portfolio/scripts/portfolio.js
@@ -22,29 +22,41 @@ thumbNailA[0].addEventListener('click',function(){
     console.log(this); //이벤트 객체 자동인식하는지 확인 -> 객체이름을 안적어도됨
     console.log(this.children[0].src); //콘솔에 배열형태로 뜨면 무조건 배열로 잡아줘야한다.
     console.log(popupBg.children[0].children[0].src); //팝업bg의 자식의 자식의 src확인
-    popupShow(); //팝업 출력 함수 호출 (함수개별호출 시 괄호 붙이기)
-    popupBg.children[0].children[0].src = this.children[0].src
+    popupShow(this); //팝업 출력 함수 호출 (함수개별호출 시 괄호 붙이기)
 }) //클릭 시(이벤트)
-
 thumbNailA[1].addEventListener('click',function(){
-    console.log(1);
-    console.log(this);
-    console.log(this.children[0].src);
-    console.log(popupBg.children[0].children[0].src);
-    popupShow();
-    popupBg.children[0].children[0].src = this.children[0].src;
+    popupShow(this);
+}) //클릭 시(이벤트)
+thumbNailA[2].addEventListener('click',function(){
+    popupShow(this);
+}) //클릭 시(이벤트)
+thumbNailA[3].addEventListener('click',function(){
+    popupShow(this);
+}) //클릭 시(이벤트)
+thumbNailA[4].addEventListener('click',function(){
+    popupShow(this);
+}) //클릭 시(이벤트)
+thumbNailA[5].addEventListener('click',function(){
+    popupShow(this);
+}) //클릭 시(이벤트)
+thumbNailA[6].addEventListener('click',function(){
+    popupShow(this);
+}) //클릭 시(이벤트)
+thumbNailA[7].addEventListener('click',function(){
+    popupShow(this);
 }) //클릭 시(이벤트)
 
-thumbNailA[2].addEventListener('click',function(){
-    console.log(this.children[0].src);
-    console.log(popupBg.children[0].children[0].src);
-    popupShow();
-    popupBg.children[0].children[0].src = this.children[0].src;
-}) //클릭 시(이벤트)
-function popupShow(){
-    return popupBg.style.display = 'flex'; //보이기 하는 함수
+function popupShow(target){
+    console.log('------------함수시작위치와 에러체크')
+    //console.log(this.children[0]); //정상체크
+    //this는 특정이벤트 안에서만 사용가능 -> 함수로 불러오는 구조로 되어있어야만 한다.
+    popupBg.style.display = 'flex'; //보이기 하는 함수
     // css에서 flex로 설정했기때문에 js에서도 flex로 작성해야한다.
     // -> 원래 가지던 속성을 그대로 가져오기
+    //popupBg.children[0].children[0].src = this.children[0].src //에러
+    // this를 매개변수로 변경해야한다.
+    popupBg.children[0].children[0].src = target.children[0].src
+    return // 함수 종료선언
 }
 //3. (팝업 실행 기준) 배경 클릭 시(popup_bg) 팝업 숨기기
 popupBg.addEventListener('click',popuphide)
